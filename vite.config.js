0e5de35ea60a3b481a0a863f803559a1a2a28d11
Costin F <costinfl@users.noreply.github.com>
2026-06-08 18:31:19 +0300

    test url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://costinfl.github.io/wc2026',
+  base: 'https://www.costinfl.dev/wc2026/',
 })
